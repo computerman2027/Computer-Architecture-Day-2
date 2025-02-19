decca5cd71a636de0efdecec3d72f20f82e5b772
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2025-02-19 15:23:25 +0530

    commit 2

--- 4 BIT UNIDIRECTIONAL BUS/UNIDIRECT_BUS_tb.v
+++ 4 BIT UNIDIRECTIONAL BUS/UNIDIRECT_BUS_tb.v
@@ -29,12 +29,10 @@ wire[3:0] o;
 UNIDIRECT_BUS uut(.inp(inp),.c(c), .o(o));
 initial begin
 
-for(integer i =0;i<16;i=i+1)
+for(inp =0;inp<16;inp=inp+1)
     begin
-        inp=i;
         c=0;
         #25;
-        //inp=i;
         c=1;
         #25;
     end
